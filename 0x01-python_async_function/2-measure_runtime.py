f2940e5689956d9fc8b89990ffb208e55e91452e
zakisu250 <zakisu250@gmail.com>
2024-01-15 16:29:24 +0300

    run multiple asyncronous courutines

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-""" Let's execute multiple coroutines at the same time with async """
-
-import time
+""" Type-annotated function that measures time of asynchronous coroutine """
 import asyncio
+from time import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n, max_delay):
-    start_time = time.time()
+def measure_time(n: int, max_delay: int) -> float:
+    """ Measures time where wait_n is being run """
+    start = time()
+    # Run starts tasks, runs until complete, and finishes
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
-    total_time = end_time - start_time
-    return total_time / n
+    end = time()
+    return (end - start) / n
