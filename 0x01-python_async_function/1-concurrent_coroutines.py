5afdba5fdbc99020c7f6d6a374899c157e7b5be0
zakisu250 <zakisu250@gmail.com>
2024-01-15 16:14:39 +0300

    Refactor wait_n function to use asyncio.gather() for concurrent coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,10 +5,22 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    delays = []
-    for future in asyncio.as_completed(tasks):
-        delay = await future
-        delays.append(delay)
-    return delays
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """Spwans wait_random n times with the specified max_delay
+    Args:
+        n: number of times to spawn wait_random
+        max_delay: the maximum delay to pass to wait_random
+    Returns:
+        A list of all the delays (float values) in ascending order
+    """
+
+    sorted_list: list[float] = []
+
+    async def randsleep(max_delay: int, sorted_list: list[float]) -> None:
+        sleep: float = await wait_random(max_delay)
+        await asyncio.sleep(sleep)
+        sorted_list.append(sleep)
+
+    await asyncio.gather(*(randsleep(max_delay,
+                                     sorted_list) for _ in range(n)))
+    return sorted_list
