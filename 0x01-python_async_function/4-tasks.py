cd67ebc39b94a7fe5f38a1095abe4c11313bc674
zakisu250 <zakisu250@gmail.com>
2024-01-15 16:30:58 +0300

    declare a longer more precise version

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Type-annotated asynchronous coroutine """
+from typing import List
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ Async function that calls task_wait_random n number of times """
+    # task_list holds coroutine objects holding floats
+    task_list: List = []
+    # complete_list holds floats
+    complete_list: List[float] = []
+
+    for x in range(n):
+        # task_wait_random asyncronously starts each task
+        task_list.append(task_wait_random(max_delay))
+    # As each task is completed, it's waited for and then appended
+    # task is just a coroutine object containing float
+    for task in asyncio.as_completed(task_list):  # <-- this line diff from 1
+        completed: float = await task
+        # Now completed can be accessed - this can't be one line
+        complete_list.append(completed)
+
+    return (complete_list)
