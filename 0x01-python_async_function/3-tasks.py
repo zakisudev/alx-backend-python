c6dd39363fab9628fa199aa71dd03938a568f29b
zakisu250 <zakisu250@gmail.com>
2024-01-15 16:30:07 +0300

    short function declare

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-""" Let's execute multiple coroutines at the same time with async """
-
+""" Type-annotated function creates asynchronous coroutine """
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay):
-    task = asyncio.create_task(wait_random(max_delay))
-    return task
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ Creates/returns task that takes random amount of time """
+    return asyncio.create_task(wait_random(max_delay))
