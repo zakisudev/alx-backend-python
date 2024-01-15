c54f7eea7dfef3a4fb9537407bc18a25a56ff74d
zakisu250 <zakisu250@gmail.com>
2024-01-15 15:58:40 +0300

    Add docstring to wait_random function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,7 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """Wait for a random delay between 0 and max_delay"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
