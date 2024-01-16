d1a50e852225a299fd84ca0d18572df6de3ceef4
zakisu250 <zakisu250@gmail.com>
2024-01-16 20:45:53 +0300

    more annotation

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
-'''
-await gather for parallel exec
-'''
-import asyncio
+"""
+2-measure_runtime.py
+"""
 import time
+import asyncio
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    '''measure async performance'''
-    st = time.perf_counter()
-    coros = [async_comprehension() for i in range(4)]
-    await asyncio.gather(*coros)
-    return time.perf_counter() - st
+    """
+    Measures the execution time of four calls to the async_comprehension
+    function using asyncio.gather.
+
+    Returns:
+        float: The total execution time in seconds.
+    """
+    start = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end = time.perf_counter()
+    return (end - start)
