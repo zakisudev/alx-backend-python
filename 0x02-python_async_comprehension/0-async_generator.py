06f54409c54d95033efe357a1474cc1c0f16f5bb
zakisu250 <zakisu250@gmail.com>
2024-01-16 20:41:24 +0300

    update documentation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 '''
-yields values after sleeping
+Asynchronously generates random floats between 0 and 10.
 '''
 
 import asyncio
 from random import uniform
-from types import NoneType
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    '''yield numbers asynchronously'''
+    '''Asynchronously generates random floats between 0 and 10.'''
     for _ in range(10):
-        # https://splunktool.com/why-does-asynciosleep0-make-my-code-faster
+        # Sleep for 1 second
         await asyncio.sleep(1)
+        # Yield a random number between 0 and 10
         yield uniform(0, 10)
